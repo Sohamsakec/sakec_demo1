9fb93ced30cbf6957fd731d03db6f8ccc5f927ed
Sohamsakec <soham.patil17572@sakec.ac.in>
2024-08-08 14:19:36 +0530

    Update demo1.groovy

--- demo1.groovy
+++ demo1.groovy
@@ -2,19 +2,12 @@ pipeline {
     agent any
 
     stages {
-        stage('Checkout') {
+        stage('Hello World') {
             steps {
-                // Checkout the code from Git repository
-                git branch: 'main', url: 'https://github.com/Sohamsakec/sakec_demo1.git'
-            }
-        }
-        stage('Build') {
-            steps {
-                // Build steps here
-                echo 'Building...'
-                // Example build step
-                withMaven(maven: 'M3') {
-                    sh 'mvn clean install'
+                script {
+                    // Groovy code to print "Hello, World!"
+                    def message = "Hello, World!"
+                    echo message
                 }
             }
         }
